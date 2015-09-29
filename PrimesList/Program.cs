5eb57b829428723d83da6cac069db5a319590432
malachi26 <lyle.hart.3@gmail.com>
2015-09-29 16:48:57 -0500

    initial Commit

---
+++ PrimesList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PrimesList
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var upperLimit = 9999;
+            var primes = new List<long>();
+            primes.Add(2);
+            for (long i = 3; i < upperLimit; i += 2)
+            {
+                primes.Add(i);
+            }
+
+            foreach(var prime in primes.ToArray())
+            {
+                for (var j = primes.Count - 1; j >= 0; j--)
+                {
+                    if (primes[j] == prime) continue;
+                    if (primes[j] % prime == 0)
+                        primes.RemoveAt(j);
+                }
+            }
+            primes.ForEach(Console.WriteLine);
+
+            Console.WriteLine("The Last Prime is " + primes[primes.Count - 1]);
+            Console.WriteLine("what are you waiting for? Exit the program!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SixSieve/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixSieve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = 1000;
+            
+            //boolean[] isPrime = new boolean [maxNumber];
+            bool[] isPrime = new bool[maxNumber];
+
+
+            isPrime[2] = true;
+            isPrime[3] = true;
+            
+            for (int i = 5, j = 7; i < maxNumber; i+=6, j+=6) {
+                isPrime[i] = true;
+            
+                if (j < maxNumber)       
+                    isPrime[j] = true;
+            }
+            
+            
+            for (int i = 5; i*i <= maxNumber; i+=2) {
+                if (isPrime[i]) {
+                    for (int j = i, z = i*i; j*i <= maxNumber; ) {
+                        if(j*i == z && z < maxNumber/2) {
+                            while (true){
+                                if (z <= maxNumber) {
+                                    isPrime[z] = false; 
+                                    z = z+(i*i*2);
+                                }
+                                else{
+                                    j+=2; break;
+                                }
+                            }
+                        } else {
+                            isPrime[j*i] = false;
+                            j+=2;
+                            while (!isPrime[j]) {
+                                j+=2;
+                            }
+                        }
+                    }
+                }   
+            }
+            Console.WriteLine("Done");
+            
+            for (int i = 0; i < maxNumber; i++) {
+                if (isPrime[i])
+                    Console.WriteLine(i + " ");
+                
+            }
+        }
+    }
+}
